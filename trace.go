47727f9e1fff9b091603173b4dc8ec2d0092d0ae
Anton Ekberg <anton.ekberg@skf.com>
2020-12-10 11:53:23 +0100

    Fix tracing nil pointer dereference

--- trace.go
+++ trace.go
@@ -14,13 +14,17 @@ type Span interface {
 	End()
 }
 
+type NilSpan struct{}
+
+func (s *NilSpan) End() {}
+
 type OpenCensusTracer struct{}
 
 func (t *OpenCensusTracer) StartSpan(ctx context.Context, resourceName string) (context.Context, Span) {
 	// Avoid creating a new trace for the middlewares, most requests will have a trace but
 	// health endpoints will not. This will avoid creating unnessesary traces for those.
 	if trace.FromContext(ctx) == nil {
-		return ctx, nil
+		return ctx, &NilSpan{}
 	}
 
 	return trace.StartSpan(ctx, resourceName)
