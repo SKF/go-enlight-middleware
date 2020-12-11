905fba58bdffe07ab8f7f31a72de4c3ba71e102f
Anton Ekberg <anton.ekberg@skf.com>
2020-12-11 16:40:45 +0100

    Add Authorization middleware (#2)

---
+++ authorization/handler.go
@@ -0,0 +1,99 @@
+package authorization
+
+import (
+	"context"
+	"errors"
+	"net/http"
+
+	"github.com/SKF/go-rest-utility/problems"
+	"github.com/SKF/go-utility/v2/log"
+	"github.com/SKF/go-utility/v2/useridcontext"
+	proto "github.com/SKF/proto/v2/common"
+	"github.com/gorilla/mux"
+
+	middleware "github.com/SKF/go-enlight-middleware"
+)
+
+type AuthorizerClient interface {
+	IsAuthorizedWithContext(ctx context.Context, userID, action string, resource *proto.Origin) (bool, error)
+	GetResourceWithContext(ctx context.Context, id, originType string) (proto.Origin, error)
+}
+
+type Middleware struct {
+	Tracer middleware.Tracer
+
+	authorizerClient AuthorizerClient
+	policies         map[*mux.Route]Policy
+}
+
+var (
+	ErrNoAuthenticationMiddleware = errors.New("unable to extract user id from context, missing authentication middleware?")
+)
+
+func New(opts ...Option) *Middleware {
+	m := &Middleware{
+		Tracer: &middleware.OpenCensusTracer{},
+
+		authorizerClient: nil,
+		policies:         map[*mux.Route]Policy{},
+	}
+
+	for _, opt := range opts {
+		opt(m)
+	}
+
+	return m
+}
+
+func (m *Middleware) SetPolicy(route *mux.Route, policy Policy) *Middleware {
+	m.policies[route] = policy
+
+	return m
+}
+
+func (m *Middleware) Middleware() func(http.Handler) http.Handler {
+	if m.authorizerClient == nil {
+		log.Warning("Unable no AuthorizerClient found in Authorization middleware, disabling authorization.")
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx, span := m.Tracer.StartSpan(r.Context(), "Middleware/Authorization")
+
+			policy, found := m.findPolicyForRequest(ctx, r)
+			if found && m.authorizerClient != nil {
+				userID, ok := useridcontext.FromContext(ctx)
+				if !ok {
+					err := ErrNoAuthenticationMiddleware
+
+					problems.WriteResponse(ctx, err, w, r)
+					span.End()
+					return
+				}
+
+				if err := policy.Authorize(ctx, userID, m.authorizerClient, r); err != nil {
+					problems.WriteResponse(ctx, err, w, r)
+					span.End()
+					return
+				}
+			}
+
+			span.End()
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+func (m *Middleware) findPolicyForRequest(ctx context.Context, r *http.Request) (Policy, bool) {
+	_, span := m.Tracer.StartSpan(ctx, "Middleware/Authorization/findPolicyForRequest")
+	defer span.End()
+
+	currentRoute := mux.CurrentRoute(r)
+	if currentRoute == nil {
+		return nil, false
+	}
+
+	policy, found := m.policies[currentRoute]
+
+	return policy, found
+}
---
+++ authorization/handler_test.go
@@ -0,0 +1,111 @@
+package authorization
+
+import (
+	"context"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	authorize_mock "github.com/SKF/go-enlight-sdk/v2/services/authorize/mock"
+	"github.com/SKF/go-rest-utility/problems"
+	"github.com/SKF/go-utility/v2/useridcontext"
+	proto "github.com/SKF/proto/v2/common"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+var (
+	userID   = "3ac91764-e57b-4a67-aa48-da99b029c04d"
+	resource = &proto.Origin{
+		Id:   "904dfc2a-7103-4561-aa45-6e5d317e90eb",
+		Type: "node",
+	}
+	policy = ActionResourcePolicy{
+		Action: "HIERARCHY::GET_NODE",
+		ResourceExtractor: func(ctx context.Context, r *http.Request) (*proto.Origin, error) {
+			return resource, nil
+		},
+	}
+)
+
+func setupAndDoRequest(userID string, policy Policy, middleware *Middleware) *http.Response {
+	endpoint := http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		w.Write([]byte(`{"message":"success"}`)) //nolint
+	})
+
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+	w := httptest.NewRecorder()
+
+	r := mux.NewRouter()
+	r.Use(func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ctx := useridcontext.NewContext(r.Context(), userID)
+			next.ServeHTTP(w, r.WithContext(ctx))
+		})
+	})
+	r.Use(middleware.Middleware())
+
+	route := r.Handle("/", endpoint)
+	middleware.SetPolicy(route, policy)
+
+	r.ServeHTTP(w, request)
+
+	return w.Result()
+}
+
+func TestValidAuthorizedRequest(t *testing.T) {
+	authorizerMock := authorize_mock.Create()
+	authorizerMock.On("IsAuthorizedWithContext", mock.Anything, userID, policy.Action, resource).Return(true, nil)
+	authorizerMock.On("GetResourceWithContext", resource.Id, resource.Type).Return(*resource, nil)
+
+	middleware := New(WithAuthorizerClient(authorizerMock))
+
+	response := setupAndDoRequest(userID, policy, middleware)
+	defer response.Body.Close()
+
+	require.Equal(t, http.StatusOK, response.StatusCode)
+}
+
+func TestUnauthorizedRequestOnExistingResource(t *testing.T) {
+	authorizerMock := authorize_mock.Create()
+	authorizerMock.On("IsAuthorizedWithContext", mock.Anything, userID, policy.Action, resource).Return(false, nil)
+	authorizerMock.On("GetResourceWithContext", resource.Id, resource.Type).Return(*resource, nil)
+
+	middleware := New(WithAuthorizerClient(authorizerMock))
+
+	response := setupAndDoRequest(userID, policy, middleware)
+	defer response.Body.Close()
+
+	require.Equal(t, http.StatusForbidden, response.StatusCode)
+
+	var problem problems.BasicProblem
+	err := json.NewDecoder(response.Body).Decode(&problem)
+	require.NoError(t, err)
+
+	require.Equal(t, "application/problem+json", response.Header.Get("Content-Type"))
+	require.Equal(t, "/problems/unauthorized-resource", problem.ProblemType())
+}
+
+func TestUnauthorizedRequestOnMissingResource(t *testing.T) {
+	authorizerMock := authorize_mock.Create()
+	authorizerMock.On("IsAuthorizedWithContext", mock.Anything, userID, policy.Action, resource).Return(false, nil)
+	authorizerMock.On("GetResourceWithContext", resource.Id, resource.Type).Return(proto.Origin{}, status.Error(codes.NotFound, "resource not found"))
+
+	middleware := New(WithAuthorizerClient(authorizerMock))
+
+	response := setupAndDoRequest(userID, policy, middleware)
+	defer response.Body.Close()
+
+	require.Equal(t, http.StatusNotFound, response.StatusCode)
+
+	var problem problems.BasicProblem
+	err := json.NewDecoder(response.Body).Decode(&problem)
+	require.NoError(t, err)
+
+	require.Equal(t, "application/problem+json", response.Header.Get("Content-Type"))
+	require.Equal(t, "/problems/resource-not-found", problem.ProblemType())
+}
