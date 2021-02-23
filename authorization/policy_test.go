bf7015e064fadb8565e803f59eb96882ac2b0780
stroem <jonathan@devies.se>
2021-02-23 13:49:13 +0100

    Added test when resource is nil

--- authorization/policy_test.go
+++ authorization/policy_test.go
@@ -4,10 +4,13 @@ import (
 	"context"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 
+	"github.com/SKF/go-enlight-sdk/v2/services/authorize"
 	authorize_mock "github.com/SKF/go-enlight-sdk/v2/services/authorize/mock"
 	proto "github.com/SKF/proto/v2/common"
+	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
 
@@ -20,8 +23,29 @@ func TestActionResourcePolicyWithOnlyAction(t *testing.T) {
 	request := httptest.NewRequest(http.MethodGet, "/", nil)
 
 	authorizerMock := authorize_mock.Create()
-
-	authorizerMock.On("IsAuthorizedWithReasonWithContext", ctx, userID, policy.Action, (*proto.Origin)(nil)).Return(true, "", nil)
+	authorizerMock.On("IsAuthorizedWithReasonWithContext", ctx, userID, policy.Action, (*proto.Origin)(nil)).
+		Return(true, "", nil).Once()
 
 	require.NoError(t, policy.Authorize(ctx, userID, authorizerMock, request))
+
+	authorizerMock.AssertExpectations(t)
+}
+
+func TestActionResourcePolicyWithOnlyAction_Unauthorized(t *testing.T) {
+	ctx := context.Background()
+	policy := ActionResourcePolicy{
+		Action:            "Action",
+		ResourceExtractor: nil,
+	}
+	request := httptest.NewRequest(http.MethodGet, "/", nil)
+
+	authorizerMock := authorize_mock.Create()
+	authorizerMock.On("IsAuthorizedWithReasonWithContext", ctx, userID, policy.Action, (*proto.Origin)(nil)).
+		Return(false, authorize.ReasonAccessDenied, nil).Once()
+
+	err := policy.Authorize(ctx, userID, authorizerMock, request)
+	require.Error(t, err)
+	assert.Equal(t, reflect.TypeOf(err).String(), "problems.UnauthorizedProblem")
+
+	authorizerMock.AssertExpectations(t)
 }
