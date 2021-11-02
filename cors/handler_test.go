90577eaa67ec9d86b12a7a7d7c6b454e40291c33
Johan Dahlberg <johan@dahlberg.co>
2021-11-02 09:29:11 +0100

    Feedback fixes

--- cors/handler_test.go
+++ cors/handler_test.go
@@ -6,12 +6,13 @@ import (
 	"net/http/httptest"
 	"testing"
 
-	cors_mw "github.com/SKF/go-enlight-middleware/cors"
 	"github.com/gorilla/mux"
 	"github.com/stretchr/testify/require"
+
+	cors_mw "github.com/SKF/go-enlight-middleware/cors"
 )
 
-func Test_Get(t *testing.T) {
+func Test_AccessControlHeaders_MethodGet(t *testing.T) {
 	options := []cors_mw.Option{}
 
 	middleware := cors_mw.New(options...)
@@ -21,10 +22,8 @@ func Test_Get(t *testing.T) {
 	response := doRequest(request, middleware)
 	defer response.Body.Close()
 
-	allowOrigin, allowOriginExist := response.Header["Access-Control-Allow-Origin"]
-	require.True(t, allowOriginExist)
-	require.Len(t, allowOrigin, 1)
-	require.Equal(t, "*", allowOrigin[0])
+	allowOrigin := response.Header.Get("Access-Control-Allow-Origin")
+	require.Equal(t, "*", allowOrigin)
 
 	var body CorsEcho
 	json.NewDecoder(response.Body).Decode(&body) // nolint
@@ -33,7 +32,7 @@ func Test_Get(t *testing.T) {
 	require.Equal(t, http.StatusOK, response.StatusCode)
 }
 
-func Test_Options(t *testing.T) {
+func Test_AccessControlHeaders_MethodOptions(t *testing.T) {
 	options := []cors_mw.Option{}
 
 	middleware := cors_mw.New(options...)
@@ -43,20 +42,14 @@ func Test_Options(t *testing.T) {
 	response := doRequest(request, middleware)
 	defer response.Body.Close()
 
-	allowHeaders, allowHeadersExist := response.Header["Access-Control-Allow-Headers"]
-	require.True(t, allowHeadersExist)
-	require.Len(t, allowHeaders, 1)
-	require.Equal(t, "*", allowHeaders[0])
+	allowHeaders := response.Header.Get("Access-Control-Allow-Headers")
+	require.Equal(t, "*", allowHeaders)
 
-	allowMethods, allowMethodsExist := response.Header["Access-Control-Allow-Methods"]
-	require.True(t, allowMethodsExist)
-	require.Len(t, allowMethods, 1)
-	require.Equal(t, "*", allowMethods[0])
+	allowMethods := response.Header.Get("Access-Control-Allow-Methods")
+	require.Equal(t, "*", allowMethods)
 
-	allowOrigin, allowOriginExist := response.Header["Access-Control-Allow-Origin"]
-	require.True(t, allowOriginExist)
-	require.Len(t, allowOrigin, 1)
-	require.Equal(t, "*", allowOrigin[0])
+	allowOrigin := response.Header.Get("Access-Control-Allow-Origin")
+	require.Equal(t, "*", allowOrigin)
 
 	body := CorsEcho{}
 	json.NewDecoder(response.Body).Decode(&body) // nolint
